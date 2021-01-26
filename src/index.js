042ed031db625f2c13872157775d34b27c234d07
Denis <osadchydenis@gmail.com>
2021-01-26 14:33:06 +0200

    trial version of app

--- src/index.js
+++ src/index.js
@@ -8,12 +8,8 @@ import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 
-const store = createStore(rootReducer, compose(
-  applyMiddleware(
-    thunk
-  ),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-))
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
 const app = (
   <BrowserRouter>
     <Provider store={store}>
