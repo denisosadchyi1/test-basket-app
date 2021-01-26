1ed18aa7a2f9ceeabe5977b72b96d221b60b9810
Denis <osadchydenis@gmail.com>
2021-01-26 15:22:27 +0200

    test commit

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import {BrowserRouter} from 'react-router-dom';
 import {rootReducer} from './redux/reducers/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, composeWithDevTools(  
+  applyMiddleware(thunk)
+))
 
 const app = (
   <BrowserRouter>
